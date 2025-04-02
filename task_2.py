3004fe601a4ad8d8fc69f259e97e0b7a48465638
chandanxrandhawa <chandanprit121@gmail.com>
2025-04-02 10:31:02 -0700

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,13 @@
+
+numbers = list(range(1, 11))
+
+
+first_five = numbers[:5]
+first_ten= numbers[:10]
+
+
+reversed_first_five = first_five[::-1]
+
+print("Original list:", first_ten)
+print("Extracted first five elements:", first_five)
+print("Reversed first five elements:", reversed_first_five)
